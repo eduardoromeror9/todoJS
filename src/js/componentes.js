92b4c1522a6e879a3914f5e5fa0139ab95ed217b
Eduardo Romero <eduardoromeror9@outlook.com>
2021-11-24 05:04:06 -0300

    Agregando filtros a la app

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -2,9 +2,11 @@ import {Todo} from '../classes';
 import {todoList} from '../index'
 
 // Referencias al HTML
-const divTodoList = document.querySelector('.todo-list');
-const txtInput    = document.querySelector('.new-todo');  
-const btnBorrar   = document.querySelector('.clear-completed');
+const divTodoList 	= document.querySelector('.todo-list');
+const txtInput    	= document.querySelector('.new-todo');  
+const btnBorrar   	= document.querySelector('.clear-completed');
+const ulFiltros   	= document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
 
 
 
@@ -81,3 +83,39 @@ btnBorrar.addEventListener('click', () => {
 	}
 
 });
+
+ulFiltros.addEventListener('click', (event) => {
+
+	const filtro = event.target.text;
+
+	if (!filtro) {
+		return;
+	}
+
+	anchorFiltros.forEach(elem => elem.classList.remove('selected'));
+	event.target.classList.add('selected');
+
+	for (const elemento of divTodoList.children) {
+
+		elemento.classList.remove('hidden');
+		const completado = elemento.classList.contains('completed');
+
+		switch (filtro) {
+			case 'Pendientes':
+				if (completado) {
+					elemento.classList.add('hidden');
+				}
+				break;
+
+			case 'Completados':
+				if (!completado) {
+					elemento.classList.add('hidden');
+				}
+				break;
+
+
+		}
+
+	}
+
+})
